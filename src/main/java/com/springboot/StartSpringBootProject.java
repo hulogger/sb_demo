0de2f7dfee48fd56d3bba773c38930cd205661a0
zzc <2329706230@qq.com>
2020-06-14 18:02:39 +0800

    老张提交

---
+++ src/main/java/com/springboot/StartSpringBootProject.java
@@ -0,0 +1,12 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartSpringBootProject {
+    public static void main(String[] args) {
+            //启动SpringBoot工程
+        SpringApplication.run(StartSpringBootProject.class);
+    }
+}
